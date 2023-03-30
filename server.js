1b40f2a34249d33622b4a60a4090c2fd802b556d
nguyensonnl <nguyenquynhson30446@gmail.com>
2023-03-30 19:29:01 +0700

    change some files

--- server.js
+++ server.js
@@ -16,29 +16,6 @@ app.get("/", (req, res) => {
   res.send("Server on");
 });
 
-//Course On Demand
-const categories1 = require("./routes/categories1");
-const product1 = require("./routes/product1");
-const brand1 = require("./routes/brand1");
-app.use("/api/v1/categories", categories1);
-app.use("/api/v1/products", product1);
-app.use("/api/v1/brands", brand1);
-
-const auth = require("./middlewares/auth1");
-const auth1 = require("./routes/auth1");
-app.use("/api/v1/auth", auth1);
-
-const customer1 = require("./routes/customer1");
-app.use("/api/v1/customers", customer1);
-
-const order1 = require("./routes/orders1");
-app.use("/api/v1/orders", order1);
-
-//Upload static files
-app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
-
-app.use(upload.array());
-
 //Port
 const port = process.env.APP_PORT || 5000;
 app.listen(port, () => {
